2d81739404631241d0ab00523e6f92f93730704b
Kaue Lima <kauelyma@gmail.com>
2026-02-04 22:03:23 -0300

    Ajusta classe Pessoa e corrige warning de null

--- Program.cs
+++ Program.cs
@@ -1,30 +1,6 @@
-﻿using System;
+﻿using DIO.Models;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Cadastro fixo
-        string emailCadastro = "anavaz@email.com";
-        string nomeUsuario = "Ana";
-        string senha = "2323";
-
-        // Entrada do usuário
-        Console.Write("Digite seu email: ");
-        string registroEmail = Console.ReadLine() ?? "";
-
-        Console.Write("Digite sua senha: ");
-        string registroSenha = Console.ReadLine() ?? "";
-
-        // Verificação
-        if (registroEmail == emailCadastro && registroSenha == senha)
-        {
-            Console.WriteLine("Login realizado com sucesso!");
-            Console.WriteLine($"Bem-vindo(a), {nomeUsuario}!");
-        }
-        else
-        {
-            Console.WriteLine("Email ou senha incorretos.");
-        }
-    }
-}
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Maria";
+pessoa1.Idade = 25;
+pessoa1.Apresentar();
--- obj/Debug/net9.0/DIO.AssemblyInfo.cs
+++ obj/Debug/net9.0/DIO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912e829a6e2390850017cb6d3a954deec30d1979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70da1f67ce6c40a39b1c05b6b513e98f2d612d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
